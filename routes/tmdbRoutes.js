f389cef33d17577818a46f691e77c78860649af7
Boris <boris.babcak@kosickaakademia.sk>
2025-01-07 12:07:40 +0100

    added endpoint for flexible filtering and added pagination

--- routes/tmdbRoutes.js
+++ routes/tmdbRoutes.js
@@ -4,6 +4,7 @@ import {
   getTrendingMovies,
   getLatestMovies,
   searchMovies,
+  discoverMovies,
   getMovieTrailers,
   getMovieImages,
   getMovieDetails
@@ -14,7 +15,8 @@ const router = express.Router();
 router.get('/genres', getGenres);
 router.get('/trending', getTrendingMovies);
 router.get('/latest', getLatestMovies);
-router.get('/search', searchMovies);
+router.get('/search', searchMovies); //by movie name/title
+router.get('/discover', discoverMovies); //flexible filtering
 router.get('/movie/:movieId/trailers', getMovieTrailers);
 router.get('/movie/:movieId/images', getMovieImages);
 router.get('/movie/:movieId', getMovieDetails);
